3ff7da3375facf7d99d296277d064e72c24cac92
flabbet <flubbet@gmail.com>
2019-12-02 21:22:32 +0100

    Leap year checker upload

---
+++ leap-year/target/debug/deps/leap_year-bb718f7a12314aa9.d
@@ -0,0 +1,5 @@
+/home/flabbet/Git/Rust GCI tasks/leap-year/target/debug/deps/leap_year-bb718f7a12314aa9: src/main.rs
+
+/home/flabbet/Git/Rust GCI tasks/leap-year/target/debug/deps/leap_year-bb718f7a12314aa9.d: src/main.rs
+
+src/main.rs:
